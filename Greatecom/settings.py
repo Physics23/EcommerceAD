cfd2c25db2f919b85a1ad92b33699feef5bbc0e6
Physics23 <laye192000@yahoo.fr>
2024-01-01 16:49:04 -0600

    wh

--- Greatecom/settings.py
+++ Greatecom/settings.py
@@ -135,10 +135,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR /'static'
-django_heroku.settings(locals())
 STATICFILES_DIRS = ['Greatecom/static',]
+
 
 # media files confugurations
 MEDIA_URL ='/media/'
