7a2cc821b1c5ed3cd648604f817feff8b2b26d06
Physics23 <laye192000@yahoo.fr>
2024-01-02 13:52:23 -0600

    m

--- Greatecom/settings.py
+++ Greatecom/settings.py
@@ -168,4 +168,4 @@ EMAIL_USE_TLS = config('EMAIL_USE_TLS', cast=bool)
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-#django_heroku.settings(locals())
+django_heroku.settings(locals())
