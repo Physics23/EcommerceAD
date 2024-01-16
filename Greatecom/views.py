3cc7b661e6d6bd0424d770700405edfadbd9698f
Physics23 <laye192000@yahoo.fr>
2024-01-16 14:00:34 -0600

    views

--- Greatecom/views.py
+++ Greatecom/views.py
@@ -5,9 +5,10 @@ def home(request):
     products = Product.objects.all().filter(is_available =True).order_by('-created_date')
     for product in products:
         reviews = ReviewRating.objects.filter(product__id = product.id )
-        context = {
-            'products': products,
-            'reviews': reviews,
-            }
+
+    context = {
+        'products': products,
+        'reviews': reviews,
+        }
 
     return render(request, 'home.html', context)
