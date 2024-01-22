f2e9e8b17a61506fe9d47690ff42aa78b7fecbd7
Physics23 <42985842+Physics23@users.noreply.github.com>
2024-01-22 12:44:16 -0600

    Update views.py

--- Greatecom/views.py
+++ Greatecom/views.py
@@ -4,12 +4,12 @@ from store.models import Product, ReviewRating
 def home(request):
     products = Product.objects.all().filter(is_available =True).order_by('-created_date')
     for product in products:
-        
         reviews = ReviewRating.objects.filter(product_id = product.id, status=True )
+        
     context = {
 
-             'reviews': reviews,
-             'products':products,
+        'reviews': reviews,
+        'products':products,
         }
 
     return render(request, 'home.html', context)
