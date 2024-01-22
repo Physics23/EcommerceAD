a6e63912e8a8ef6e377e08e3c5d8939ee0c56710
Physics23 <42985842+Physics23@users.noreply.github.com>
2024-01-22 12:40:39 -0600

    Update views.py

--- Greatecom/views.py
+++ Greatecom/views.py
@@ -5,7 +5,7 @@ def home(request):
     products = Product.objects.all().filter(is_available =True).order_by('-created_date')
     for product in products:
         reviews = ReviewRating.objects.filter(product_id = product.id, status=True )
-        context = {
+    context = {
 
             'reviews': reviews,
              'products':products,
