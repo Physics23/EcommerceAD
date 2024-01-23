fecef38b765d855607b2b4aafd35322f24ab2738
Physics23 <laye192000@yahoo.fr>
2024-01-23 13:16:39 -0600

    greatcom

--- Greatecom/views.py
+++ Greatecom/views.py
@@ -4,13 +4,13 @@ from store.models import Product, ReviewRating
 
 
 def home(request):
-    global reviews
-    global context
-    global products
     products = Product.objects.all().filter(is_available =True).order_by('-created_date')
     for product in products:
-        reviews = ReviewRating.objects.filter(product_id = product.id, status=True )
+        reviews = ReviewRating.objects.filter(product_id=product.id, status=True)
 
-    context = {'products': products , 'reviews':reviews,}
+    context = {
+    'products': products,
+    'reviews':reviews,
+    }
 
     return render(request, 'home.html', context)
