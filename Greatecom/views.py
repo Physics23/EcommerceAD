4cbce38faae36a337da1f3d519e60f491315a9d2
Physics23 <laye192000@yahoo.fr>
2024-01-23 12:22:34 -0600

    greatcom

--- Greatecom/views.py
+++ Greatecom/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
 from store.models import Product, ReviewRating
 
-def home(request, product_id):
+def home(request):
     products = Product.objects.all().filter(is_available =True).order_by('-created_date')
-    reviews = ReviewRating.objects.filter(product.id == product_id, status=True )
+    reviews = ReviewRating.objects.filter(product.id=product_id, status=True )
     for product in products:
         # reviews = ReviewRating.objects.filter(product_id = product.id, status=True )
 
