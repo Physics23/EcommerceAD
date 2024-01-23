d93cbc0492aa74700ad018670ced5aa4544e8271
Physics23 <laye192000@yahoo.fr>
2024-01-23 12:34:48 -0600

    greatcom

--- Greatecom/views.py
+++ Greatecom/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from store.models import Product, ReviewRating
 
-global reviews
+global reviews, context
 
 def home(request):
     products = Product.objects.all().filter(is_available =True).order_by('-created_date')
